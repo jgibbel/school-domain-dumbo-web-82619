000c5764948acd02ec58c8ec46134bacabcd6902
James Gibbel <jamesgibbel@gmail.com>
2019-08-28 20:56:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School 
+  attr_accessor :roster 
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name 
+    else 
+       @roster[grade] = [name]
+    end 
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+
+  def sort
+    sorted=@roster.sort_by { |key| key }.to_h
+    sorteder=sorted.each do |k,v| 
+      v.sort_by {|x| x}
+    end
+    sorteder
+  end
+
+end 
